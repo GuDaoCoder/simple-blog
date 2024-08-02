bb3777cc49d6f11b5620983f4cbae4bb1f187636
zouzhangpeng <zouzhangpeng@163.com>
2024-08-02 16:24:23 +0800

    全局异常

---
+++ backend/src/main/java/com/github/common/exception/InvalidCredentialsException.java
@@ -0,0 +1,22 @@
+package com.github.common.exception;
+
+import org.slf4j.helpers.MessageFormatter;
+
+import java.io.Serial;
+
+/**
+ * 认证信息无效异常
+ *
+ * @author Gudao
+ * @since 2024/8/2
+ */
+public class InvalidCredentialsException extends RuntimeException {
+
+	@Serial
+	private static final long serialVersionUID = -3516586981306583668L;
+
+	public InvalidCredentialsException(String errorMsg, Object... arguments) {
+		super(MessageFormatter.arrayFormat(errorMsg, arguments).getMessage());
+	}
+
+}
