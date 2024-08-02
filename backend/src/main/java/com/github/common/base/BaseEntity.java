cb0c7b760eb52e36db6992875ccff13553d7f54e
zouzhangpeng <zouzhangpeng@163.com>
2024-08-02 18:03:19 +0800

    BaseEntity添加审计功能

--- backend/src/main/java/com/github/common/base/BaseEntity.java
+++ backend/src/main/java/com/github/common/base/BaseEntity.java
@@ -1,6 +1,9 @@
 package com.github.common.base;
 
+import com.github.common.context.UserDetailContext;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -38,5 +41,31 @@ public abstract class BaseEntity implements Serializable {
 	 * 更新时间
 	 */
 	private LocalDateTime updateTime;
+
+	@PrePersist
+	public void prePersist() {
+		if (createTime == null) {
+			createTime = LocalDateTime.now();
+		}
+		if (updateTime == null) {
+			updateTime = LocalDateTime.now();
+		}
+		if (createBy == null) {
+			createBy = UserDetailContext.get().getUserId();
+		}
+		if (updateBy == null) {
+			updateBy = UserDetailContext.get().getUserId();
+		}
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		if (createTime == null) {
+			updateTime = LocalDateTime.now();
+		}
+		if (updateBy == null) {
+			updateBy = UserDetailContext.get().getUserId();
+		}
+	}
 
 }
