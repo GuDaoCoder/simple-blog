63253cf362c6e850f5396d36513fabd5f5464794
zouzhangpeng <zouzhangpeng@163.com>
2024-08-26 18:23:32 +0800

    新增存储策略、本地存储配置前缀

--- backend/src/main/java/com/blog/biz/constant/ConfigPrefixConstants.java
+++ backend/src/main/java/com/blog/biz/constant/ConfigPrefixConstants.java
@@ -11,4 +11,14 @@ public class ConfigPrefixConstants {
 	 */
 	public static final String GIT = "git.";
 
+	/**
+	 * 存储策略
+	 */
+	public static final String STORAGE = "storage.";
+
+	/**
+	 * 本地存储配置
+	 */
+	public static final String LOCAL_STORAGE_POLICY = "localStoragePolicy.";
+
 }
