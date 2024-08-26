6bb281673d3c26adc879684c861be6a7c236b78f
zouzhangpeng <zouzhangpeng@163.com>
2024-08-26 18:24:19 +0800

    新增文件工具类

---
+++ backend/src/main/java/com/blog/common/util/FileUtil.java
@@ -0,0 +1,40 @@
+package com.blog.common.util;
+
+import org.springframework.util.Assert;
+
+import java.io.File;
+
+/**
+ * @author Gudao
+ * @since 2024/8/17
+ */
+public class FileUtil {
+
+	/**
+	 * 获取文件大小，单位KB
+	 * @param file
+	 * @return long
+	 **/
+	public static long getFileKbSize(File file) {
+		Assert.notNull(file, "The file cannot be null");
+		return (file.length() + 1023) / 1024;
+	}
+
+	/**
+	 * 获取不带后缀的文件名
+	 * @param fileName
+	 * @return String
+	 **/
+	public static String getFileNameWithoutExtension(String fileName) {
+		if (fileName == null || fileName.isEmpty()) {
+			return fileName;
+		}
+		int dotIndex = fileName.lastIndexOf('.');
+
+		if (dotIndex == -1 || dotIndex == 0) {
+			return fileName;
+		}
+		return fileName.substring(0, dotIndex);
+	}
+
+}
