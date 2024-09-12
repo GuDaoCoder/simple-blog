fab034904d0a571e892db8b51595adffcbd38b29
zouzhangpeng <zouzhangpeng@163.com>
2024-09-12 18:00:23 +0800

    src下各资源文件增加别名

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,12 @@ export default defineConfig({
   plugins: [vue(), vueJsx(), autoImportPlugin('vite')],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)), // 源码根目录
+      '@style': fileURLToPath(new URL('./src/style', import.meta.url)), // 样式
+      '@img': fileURLToPath(new URL('./src/assets/img', import.meta.url)), // 图片
+      '@plugins': fileURLToPath(new URL('./src/plugins', import.meta.url)), // 本地插件
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)), // 公共组件
+      '@views': fileURLToPath(new URL('./src/views', import.meta.url)) // 路由组件
     }
   }
 })
