341a344c9f017bf0aaf31a3591105cec6210b2f0
zouzhangpeng <zouzhangpeng@163.com>
2024-09-30 23:16:29 +0800

    更换主题

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,6 +5,10 @@ import router from '@/router'
 import store from '@/store'
 import 'virtual:windi.css'
 
-const app = createApp(App)
+// 主题
+import TinyThemeTool from '@opentiny/vue-theme/theme-tool'
+import { tinySmbTheme } from '@opentiny/vue-theme/theme'
 
+const app = createApp(App)
+app.config.globalProperties.theme = new TinyThemeTool(tinySmbTheme, 'tinyStyleSheetId')
 app.use(store).use(router).mount('#app')
